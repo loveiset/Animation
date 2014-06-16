51bdbee2e8c657a008694285e9a2d72c1406089d
loveiset <loveiset@qq.com>
2014-06-16 19:17:02 +0800

    第二章
    导入fbx

---
+++ Assets/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f,-45f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+    Vector3 camRot;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log(Vector3.up);
+        Debug.Log(Quaternion.identity);
+        camRot = Camera.main.transform.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //camRot.y -= Input.GetAxis("Mouse X");
+        //camRot.x += Input.GetAxis("Mouse Y");
+        camRot.x = 50;
+        //Debug.Log("x" + Input.GetAxis("Mouse X"));
+        //Debug.Log("y" + Input.GetAxis("Mouse Y"));
+        Camera.main.transform.eulerAngles = camRot;
+        Debug.Log("transx"+Camera.main.transform.eulerAngles.x);
+        this.transform.Rotate(Time.deltaTime * 30, Time.deltaTime * 30, Time.deltaTime * 30, Space.Self);
+	}
+}
